bf200a20172aab86cc76637d0a7bf60be5a6a92e
Jawad Alrajhi <Jawadalrajhi@hotmail.com>
2025-11-03 13:01:04 +0300

    Implement find_path function for command resolution
    Added function to find the full path of a command.

--- path.c
+++ path.c
@@ -1,1 +1,13 @@
-/* File intentionally left empty for structure setup */
+#include "shell.h"
+
+/**
+ * find_path - Finds the full path of a command
+ * @command: Command to search for
+ *
+ * Return: Pointer to full path string or NULL if not found
+ */
+char *find_path(char *command)
+{
+    /* TODO: Implement PATH resolution logic (by Nawaf) */
+    return (NULL);
+}
