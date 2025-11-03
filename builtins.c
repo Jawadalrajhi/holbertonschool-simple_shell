298e1d917d81ec59b59fbb0b1aae03da1817cb6e
Jawad Alrajhi <Jawadalrajhi@hotmail.com>
2025-11-03 13:01:26 +0300

    Add handle_exit and handle_env functions
    Implemented built-in commands for exit and env.

--- builtins.c
+++ builtins.c
@@ -1,1 +1,26 @@
-/* File intentionally left empty for structure setup */
+#include "shell.h"
+
+/**
+ * handle_exit - Handles the built-in exit command
+ * @input: User input string
+ *
+ * Return: void
+ */
+void handle_exit(char *input)
+{
+    if (strcmp(input, "exit") == 0)
+        exit(0);
+}
+
+/**
+ * handle_env - Handles the built-in env command
+ *
+ * Return: void
+ */
+void handle_env(void)
+{
+    int i;
+
+    for (i = 0; environ[i]; i++)
+        printf("%s\n", environ[i]);
+}
