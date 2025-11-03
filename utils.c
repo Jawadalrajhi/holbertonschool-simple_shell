c809a59fbfb7b7b8910e4fe12317edb08e783531
Jawad Alrajhi <Jawadalrajhi@hotmail.com>
2025-11-03 13:00:42 +0300

    Implement tokenize_input and free_tokens functions
    Added functions to tokenize input and free tokens.

--- utils.c
+++ utils.c
@@ -1,1 +1,24 @@
-/* File intentionally left empty for structure setup */
+#include "shell.h"
+
+/**
+ * tokenize_input - Splits input string into tokens
+ * @input: The input string to split
+ *
+ * Return: Array of token strings
+ */
+char **tokenize_input(char *input)
+{
+    /* TODO: Implement argument parsing later */
+    return (NULL);
+}
+
+/**
+ * free_tokens - Frees memory allocated for tokens
+ * @tokens: Array of strings to free
+ *
+ * Return: void
+ */
+void free_tokens(char **tokens)
+{
+    /* TODO: Free memory for tokens later */
+}
