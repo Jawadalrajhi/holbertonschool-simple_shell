761679ec67f26f9ba27f04958877d39effca217a
Jawad Alrajhi <Jawadalrajhi@hotmail.com>
2025-11-03 13:02:45 +0300

    Implement main function for shell program
    Added main function for simple shell program.

--- main.c
+++ main.c
@@ -1,1 +1,12 @@
-/* File intentionally left empty for structure setup */
+#include "shell.h"
+
+/**
+ * main - Entry point for the simple shell program
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+    /* TODO: Add shell loop later by Nawaf */
+    return (0);
+}
