45bf189154074c22d1f772058fb33e9f24382436
Jawad Alrajhi <Jawadalrajhi@hotmail.com>
2025-11-03 13:02:27 +0300

    Implement execute_command function stub
    Added function to execute a given command.

--- exec.c
+++ exec.c
@@ -1,1 +1,12 @@
-/* File intentionally left empty for structure setup */
+#include "shell.h"
+
+/**
+ * execute_command - Executes a given command
+ * @command: The command to execute
+ *
+ * Return: void
+ */
+void execute_command(char *command)
+{
+    /* TODO: Implement command execution logic (by Nawaf) */
+}
