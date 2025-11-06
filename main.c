bcf24830a07bb4a9dc4c7f27cfdd1ac7285ce555
Nawaf Saleh <128853300+NSF9@users.noreply.github.com>
2025-11-06 12:57:46 +0300

    Update main.c

--- main.c
+++ main.c
@@ -7,6 +7,46 @@
  */
 int main(void)
 {
-    /* TODO: Add shell loop later by Nawaf */
-    return (0);
+     char *input;
+    char **args;
+
+    while (1)
+    {
+        display_prompt();
+        input = read_input();
+        if (!input)
+        {
+            write(STDOUT_FILENO, "\n", 1);
+            break;
+        }
+
+        /* إزالة المسافات */
+        input[strcspn(input, "\n")] = '\0';
+        if (strlen(input) == 0)
+        {
+            free(input);
+            continue;
+        }
+
+        if (strcmp(input, "exit") == 0)
+        {
+            handle_exit(input);
+        }
+
+        if (strcmp(input, "env") == 0)
+        {
+            handle_env();
+            free(input);
+            continue;
+        }
+
+        args = tokenize_input(input);
+        if (args && args[0])
+            execute_command(args[0]);
+
+        free_tokens(args);
+        free(input);
+    }
+
+    return 0;
 }
