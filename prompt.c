9c347469fb919a04d1080384edf10c814cc7d936
Nawaf Saleh <128853300+NSF9@users.noreply.github.com>
2025-11-10 16:20:33 +0300

    Update prompt.c

--- prompt.c
+++ prompt.c
@@ -1,11 +1,33 @@
 #include "shell.h"
 
 /**
- * display_prompt - Displays the shell prompt
- *
- * Return: void
+ * display_prompt - prints the shell prompt
  */
 void display_prompt(void)
 {
-    write(STDOUT_FILENO, "#cisfun$ ", 9);
+	if (isatty(STDIN_FILENO))
+		write(STDOUT_FILENO, "$ ", 2);
 }
+
+/**
+ * get_cmd - reads input from stdin
+ * @buffer: pointer to store input string
+ * @size: pointer to buffer size
+ *
+ * Return: 0 on success, -1 on failure
+ */
+int get_cmd(char **buffer, size_t *size)
+{
+	ssize_t read_bytes;
+
+	read_bytes = getline(buffer, size, stdin);
+
+	if (read_bytes == -1)
+		return (-1);
+
+	if ((*buffer)[read_bytes - 1] == '\n')
+		(*buffer)[read_bytes - 1] = '\0';
+
+	return (0);
+}
+
