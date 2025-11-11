a35cfd7173da50da200fce24ed63afc59f801ebf
Jawad Alrajhi <Jawadalrajhi@hotmail.com>
2025-11-11 10:47:01 +0300

    Add last command exit status declaration

--- shell.h
+++ shell.h
@@ -1,12 +1,16 @@
 #ifndef SHELL_H
 #define SHELL_H
 #define SHELL_EXIT_SIGNAL 2000
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
 #include <sys/types.h>
 #include <sys/wait.h>
+
+/* last command exit status (declared in main.c) */
+extern int last_status;
 
 /**
  * struct builtin_cmd - Struct for built-in commands
